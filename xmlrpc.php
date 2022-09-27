ee1a4e556545a3e98d14364bfa44cd5bcc0fc566
digtelComp <114468401+digtelComp@users.noreply.github.com>
2022-09-27 14:59:23 +0700

    p

--- xmlrpc.php
+++ xmlrpc.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://daftar-indihome.web.id</homePageLink>
+		<homePageLink>https://digtelcomp.github.io</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://daftar-indihome.web.id/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://daftar-indihome.web.id/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://daftar-indihome.web.id/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://daftar-indihome.web.id/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://daftar-indihome.web.id/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://digtelcomp.github.io/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://digtelcomp.github.io/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://digtelcomp.github.io/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://digtelcomp.github.io/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://digtelcomp.github.io/wp-json/" />
 			</apis>
 	</service>
 </rsd>
